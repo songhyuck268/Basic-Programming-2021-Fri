2c5a26ffce4851462555ebf63515f2455f547211
songhyuck268 <82755134+songhyuck268@users.noreply.github.com>
2021-05-07 13:39:41 +0900

    Add files via upload

--- Sanghyeok/Baekjoon_10869_Sanghyeok.cpp
+++ Sanghyeok/Baekjoon_10869_Sanghyeok.cpp
@@ -5,9 +5,10 @@ int main(void)
     int a = 0, b = 0;
 
     scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
-    printf("%d\n", a - b);
-    printf("%d\n", a * b);
-    printf("%d\n", a / b);
-    printf("%d\n", a % b);
+    if (a > b)
+        printf(">");
+    else if (a < b)
+        printf("<");
+    else if (a == b)
+        printf("==");
 }
