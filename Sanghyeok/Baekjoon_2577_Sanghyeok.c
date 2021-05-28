1770ed5254fb5d57cca36edb0aa5381da3d06c22
songhyuck268 <82755134+songhyuck268@users.noreply.github.com>
2021-05-28 18:51:00 +0900

    Add files via upload

---
+++ Sanghyeok/Baekjoon_2577_Sanghyeok.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 0, c = 0, gob = 0, i = 0;
+    int arr[10] = { 0 };
+    scanf("%d %d %d", &a, &b, &c);
+    gob = a * b * c;
+    while (gob > 0)
+    {
+        i = gob % 10;
+        arr[i]++;
+        gob = gob / 10;
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+}
