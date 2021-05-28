b32da3d46a55580d3c5138bdac5d575186077b2d
songhyuck268 <82755134+songhyuck268@users.noreply.github.com>
2021-05-28 19:04:15 +0900

    Add files via upload

---
+++ Sanghyeok/Baekjoon_8958_Sanghyeok.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int num = 0, a = 0, i = 0, sum = 0, add = 1;
+    char arr[100];
+
+    scanf("%d", &num);
+    for (i = 0; i < num; i++)
+    {
+        sum = 0, add = 1;
+        scanf("%s", &arr);
+        for (a = 0; a < strlen(arr); a++)
+        {
+            if (arr[a] == 'O')
+            {
+                sum = sum + add;
+                add++;
+            }
+            else
+                add = 1;
+        }
+        printf("%d\n", sum);
+    }
+}
