becc6b7b0013347eb45233bee63fa618cb13669f
songhyuck268 <82755134+songhyuck268@users.noreply.github.com>
2021-05-28 19:19:04 +0900

    Add files via upload

---
+++ Sanghyeok/Baekjoon_10809_Sanghyeok.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int i = 0, a = 0;
+    char arr[100] = { 0 };
+    scanf("%s", &arr);
+    for (i = 97; i <= 122; i++)
+    {
+        a = 0;
+        while (arr[a] != 0)
+        {
+            if (arr[a] == (char)i)
+                break;
+            a++;
+        }
+        if (arr[a] == (char)i)
+            printf("%d ", a);
+        else
+            printf("-1 ");
+    }
+}
