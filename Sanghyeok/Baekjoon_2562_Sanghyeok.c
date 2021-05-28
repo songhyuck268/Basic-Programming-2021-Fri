5c5045a03af775f46421e63150737b90357e9600
songhyuck268 <82755134+songhyuck268@users.noreply.github.com>
2021-05-28 18:43:40 +0900

    Add files via upload

---
+++ Sanghyeok/Baekjoon_2562_Sanghyeok.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int i = 0, max = 0, num = 0;
+    int arr[9] = { 0 };
+
+    for (i = 0; i < 9; i++)
+    {
+        scanf("%d", &arr[i]);
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            num = i;
+        }
+    }
+    printf("%d\n%d", max, num + 1);
+}
